7ad2740659d5d71da27dacf8b93c7dc61c1c0420
USER <USER@INET-10>
2018-03-16 16:56:45 +0900

    re:

--- asdf2/src/chapter/Account.java
+++ asdf2/src/chapter/Account.java
@@ -21,7 +21,7 @@ public class Account {
 		if (balance == 0) {
 			System.out.println("0보다 큰값을 입력하십시오");
 		} else if (balance > 0) {
-			System.out.println("출력");
+			this.balance = balance;
 		}
 	}
 }
--- asdf2/src/chapter/AccountTest.java
+++ asdf2/src/chapter/AccountTest.java
@@ -7,7 +7,9 @@ public class AccountTest {
 	public static void main(String[] args) {
 		Account obj = new Account();
 		Scanner scan = new Scanner(System.in);
-		System.out.println("숫자를 입력해 주십시오 : ");
+		System.out.println("이름을 입력해 주십시오 :");
+		obj.setName(scan.next());
+		System.out.print("잔액을 입력해 주십시오 : ");
 		obj.setBalance(scan.nextInt());
 		System.out.println(obj.getBalance());
 	}
