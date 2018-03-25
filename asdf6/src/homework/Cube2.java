7569d71cca482ab19d6748852b7cf86db5993112
Kim-Tae-Hoon <37293094+Kim-Tae-Hoon@users.noreply.github.com>
2018-03-25 18:54:18 +0900

    Create Cube2.java

---
+++ asdf6/src/homework/Cube2.java
@@ -0,0 +1,18 @@
+// (2)
+public class Cube {
+	private double side;
+	public Cube()
+	{
+		side = 0;
+	}
+	public  Cube(double Side)
+	{
+		this.side = side;
+	}
+	public double getSide() {
+		return side;
+	}
+	publice double getVolume() {
+		retrun side*side*side;
+	}
+}
