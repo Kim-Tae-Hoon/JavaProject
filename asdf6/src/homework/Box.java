3519c0b19718b755764eee862f00002c6c243988
Kim-Tae-Hoon <37293094+Kim-Tae-Hoon@users.noreply.github.com>
2018-03-25 20:28:03 +0900

    Create Box.java

---
+++ asdf6/src/homework/Box.java
@@ -0,0 +1,45 @@
+public class Box{
+	private int width, length, height;
+	private boolean empty = false;
+	public int getWidth(){
+		retrun width;
+	}
+	public int getWidth(){
+		retrun width;
+	}
+	public int setWidth(int width){
+		this.width = width;
+	}
+	public int getLength(){
+		retrun length;
+	}
+	public int setLength(int length){
+		this.length = length;
+	}
+	public int getHeight(){
+		retrun height;
+	}
+	public int setHHeight(int height){
+		this.height = height;
+	}
+	public boolean isEmpty(){
+		retrun isEmpty;
+	}
+	public int setEmpty(boolean empty){
+		this.empty = empty;
+	}
+	public Box()
+	{
+		width = 0;
+		length = 0;
+		height = 0;
+		empty = true;
+	}
+	public Box(int w, int l, int h)
+	{
+		width = w;
+		length = l;
+		height = h;
+		empty = true;
+	}
+}
