16c1393eb07b4b5fb8157ad31f5969d8465efc0f
Kim-Tae-Hoon <37293094+Kim-Tae-Hoon@users.noreply.github.com>
2018-03-25 19:03:35 +0900

    Create SingleTest.java

---
+++ asdf6/src/homework/SingleTest.java
@@ -0,0 +1,15 @@
+class Single {
+	private static Single s_instance;
+	public static Single getInstance() {
+		if (S_instance == null) {
+			s_instance = new Single();
+		}
+		return s_instance;
+	}
+}
+public class SingleTest {
+	public static void main (String [] args) {
+		Single obj1 = Single.getInstance();
+		Single obj2 = Single.getInstance();
+	}
+}
