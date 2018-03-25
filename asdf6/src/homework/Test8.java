05c61d9a7cd3b3efa9224c778d79731c818cd4e8
Kim-Tae-Hoon <37293094+Kim-Tae-Hoon@users.noreply.github.com>
2018-03-25 18:33:34 +0900

    Update Test8.java

--- asdf6/src/homework/Test8.java
+++ asdf6/src/homework/Test8.java
@@ -1,14 +1,32 @@
+// public class Test
+// {
+//   public static void main (String [] args)
+//   {
+//     Object obj = new Object()
+//     {
+//       public boolean equals(Object obj)
+//       {
+//         return true;
+//       }
+//     }
+    
+//     System.out.println(obj.equals("Hello"));
+    
+//   }
+// }
+//--- 오류 ---
+
 public class Test
 {
   public static void main (String [] args)
   {
     Object obj = new Object()
     {
-      public boolean equals(Object boj)
+      public boolean equals(Object obj)
       {
         return true;
       }
-    }
+    };
     
     System.out.println(obj.equals("Hello"));
     
