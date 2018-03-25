d1905c35fc47c1e5cfe05da6514bd3ccab7f80fb
Kim-Tae-Hoon <37293094+Kim-Tae-Hoon@users.noreply.github.com>
2018-03-25 18:24:56 +0900

    Create Test6.java

---
+++ asdf6/src/homework/Test6.java
@@ -0,0 +1,23 @@
+public class Test
+{
+  int s;
+  public static void main(Sting[] args)
+  {
+    Test p = new Test();
+    p.sub();
+  }
+  
+  void sub
+  {
+    int x = 5;
+    setDouble(x);
+    System.out.println(x + "");
+    System.out.println(s);
+  }
+  
+  void setDouble(int x)
+  {
+    x = x*2;
+    s = x;
+  }
+}
